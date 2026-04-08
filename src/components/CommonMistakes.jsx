59b1ede3ca37c30f05c339c5e318d83410be7fa3
SweetSea <106439598+SweetSea-ButImNotSweet@users.noreply.github.com>
2026-04-09 00:35:07 +0700

    33 Finish

--- src/components/CommonMistakes.jsx
+++ src/components/CommonMistakes.jsx
@@ -49,22 +49,64 @@ export default function CommonMistakes() {
                     </div>
                 </div>
 
-                {/* 2. Wrong Channel */}
+                {/* 2. Misaligned Digits */}
                 <div className="cm-card">
                     <div className="cm-header">
-                        <MapPinOff className="w-5 h-5" />
+                        <ScanText className="w-5 h-5" />
                         <T i18nKey="cm_2_title" className="cm-title" as="h3" />
                     </div>
                     <T i18nKey="cm_2_desc" className="cm-desc" as="p" />
+
+                    <div className="space-y-2">
+                        <FakeMessage>
+                            <div className="mr-3"><Avatar /></div>
+                            <span className="text-[#f2f3f5] font-bold mr-1">/link</span>
+                            <ParameterBox styleType="cm" showLabel={true} value={
+                                <><span className="cm-error-text">I</span><span className="cm-error-text">O</span>1234<span className="cm-error-text">/</span>567<span className="cm-error-text">O</span></>
+                            } />
+                        </FakeMessage>
+
+                        <div className="flex flex-col">
+                            <FakeMessage>
+                                <div className="mr-3"><Avatar /></div>
+                                <span className="text-[#f2f3f5] font-bold mr-1">/link</span>
+                                <ParameterBox styleType="cm" showLabel={false} value={
+                                    <span className="cm-error-text">12321232123</span>
+                                } />
+                            </FakeMessage>
+                            <T i18nKey="cm_error_18_digits" className="text-[#f23f43] text-[12px] font-bold mt-1 ml-13" />
+                        </div>
+                    </div>
                 </div>
 
-                {/* 3. Formatting Errors */}
+                {/* 3. Wrong Channel */}
                 <div className="cm-card">
                     <div className="cm-header">
-                        <Type className="w-5 h-5" />
+                        <MapPinOff className="w-5 h-5" />
                         <T i18nKey="cm_3_title" className="cm-title" as="h3" />
                     </div>
                     <T i18nKey="cm_3_desc" className="cm-desc" as="p" />
+                </div>
+
+                {/* 4. Copycat */}
+                <div className="cm-card">
+                    <div className="cm-header">
+                        <CopyX className="w-5 h-5" />
+                        <T i18nKey="cm_4_title" className="cm-title" as="h3" />
+                    </div>
+                    <T i18nKey="cm_4_desc" className="cm-desc" as="p" />
+                    <div className="text-center font-bold text-red-500/80">
+                        <Frown className="w-8 h-8 mx-auto mb-1" />
+                    </div>
+                </div>
+
+                {/* 5. Formatting Errors */}
+                <div className="cm-card">
+                    <div className="cm-header">
+                        <Type className="w-5 h-5" />
+                        <T i18nKey="cm_5_title" className="cm-title" as="h3" />
+                    </div>
+                    <T i18nKey="cm_5_desc" className="cm-desc" as="p" />
 
                     <div className="space-y-2">
                         <FakeMessage>
@@ -103,43 +145,13 @@ export default function CommonMistakes() {
                     </div>
                 </div>
 
-                {/* 4. Misaligned Digits */}
-                <div className="cm-card">
-                    <div className="cm-header">
-                        <ScanText className="w-5 h-5" />
-                        <T i18nKey="cm_4_title" className="cm-title" as="h3" />
-                    </div>
-                    <T i18nKey="cm_4_desc" className="cm-desc" as="p" />
-
-                    <div className="space-y-2">
-                        <FakeMessage>
-                            <div className="mr-3"><Avatar /></div>
-                            <span className="text-[#f2f3f5] font-bold mr-1">/link</span>
-                            <ParameterBox styleType="cm" showLabel={true} value={
-                                <><span className="cm-error-text">I</span><span className="cm-error-text">O</span>1234<span className="cm-error-text">/</span>567<span className="cm-error-text">O</span></>
-                            } />
-                        </FakeMessage>
-
-                        <div className="flex flex-col">
-                            <FakeMessage>
-                                <div className="mr-3"><Avatar /></div>
-                                <span className="text-[#f2f3f5] font-bold mr-1">/link</span>
-                                <ParameterBox styleType="cm" showLabel={false} value={
-                                    <span className="cm-error-text">12321232123</span>
-                                } />
-                            </FakeMessage>
-                            <T i18nKey="cm_error_18_digits" className="text-[#f23f43] text-[12px] font-bold mt-1 ml-13" />
-                        </div>
-                    </div>
-                </div>
-
-                {/* 5. Invalid Data Fallacy */}
+                {/* 6. Invalid Data Fallacy */}
                 <div className="cm-card">
                     <div className="cm-header">
                         <Hash className="w-5 h-5" />
-                        <T i18nKey="cm_5_title" className="cm-title" as="h3" />
+                        <T i18nKey="cm_6_title" className="cm-title" as="h3" />
                     </div>
-                    <T i18nKey="cm_5_desc" className="cm-desc" as="p" />
+                    <T i18nKey="cm_6_desc" className="cm-desc" as="p" />
 
                     <div className="space-y-2">
                         <FakeMessage>
@@ -160,50 +172,14 @@ export default function CommonMistakes() {
                     </div>
                 </div>
 
-                {/* 6. Wrong Game ID */}
+                {/* 7. Wrong Game ID */}
                 <div className="cm-card">
                     <div className="cm-header">
                         <Globe2 className="w-5 h-5" />
-                        <T i18nKey="cm_6_title" className="cm-title" as="h3" />
-                    </div>
-                    <T i18nKey="cm_6_desc" className="cm-desc" as="p" />
-                    <FakeMessage>
-                        <div className="mr-3"><Avatar /></div>
-                        <span className="text-[#f2f3f5] font-bold mr-1">/link</span>
-                        <ParameterBox styleType="cm" showLabel={true} value={
-                            <span className="cm-error-text">444333222111</span>
-                        } />
-                    </FakeMessage>
-                </div>
-
-                {/* 7. Uninitialized */}
-                <div className="cm-card">
-                    <div className="cm-header">
-                        <Minus className="w-5 h-5" />
                         <T i18nKey="cm_7_title" className="cm-title" as="h3" />
                     </div>
-                    <T i18nKey="cm_7_desc" className="cm-desc" as="p" />
-                    <FakeMessage>
-                        <div className="mr-3"><Avatar /></div>
-                        <span className="text-[#f2f3f5] font-bold mr-1">/link</span>
-                        <ParameterBox styleType="cm" showLabel={true} value={
-                            <span className="cm-error-text">-</span>
-                        } />
-                    </FakeMessage>
+                    <T i18nKey="cm_7_desc" className="cm-desc mb-0!" as="p" />
                 </div>
-
-                {/* 8. Copycat */}
-                <div className="cm-card lg:col-start-2 lg:col-span-1">
-                    <div className="cm-header">
-                        <CopyX className="w-5 h-5" />
-                        <T i18nKey="cm_8_title" className="cm-title" as="h3" />
-                    </div>
-                    <T i18nKey="cm_8_desc" className="cm-desc" as="p" />
-                    <div className="text-center font-bold text-red-500/80">
-                        <Frown className="w-8 h-8 mx-auto mb-1" />
-                    </div>
-                </div>
-
             </div>
         </div>
     );
